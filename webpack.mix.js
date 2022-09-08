6f48d391875e10917923c3a4abfb0824427af856
bijit027 <debbijit70@gmail.com>
2022-09-08 11:41:20 +0600

    remove admin.js from assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,10 +4,5 @@ mix.setPublicPath("assets");
 mix.setResourceRoot('../');
 mix
   .js("src/main.js", "assets/admin/admin.js")
-  // .js('src/frontend/ems_frontend.js', 'assets/frontend/js/ems_frontend.js')
-  // .js('src/frontend/bootstrap.min.js', 'assets/Bootstrap/bootstrap.min.js')
-  // .css('src/frontend/bootstrap.min.css', 'assets/Bootstrap/bootstrap.min.css')
-  // .css('src/frontend/frontend.css', 'assets/frontend/css/frontend.css')
-  // .css("src/css/index.css", "assets/elementPlus/index.css")
   .vue({ version: 3 })
   .sourceMaps(false);
